b60f3837aea5d7a1860a410f3aa16b5b11d1639d
unknown <eky5074@psu.edu>
2025-01-06 16:46:10 -0500

    syntax

--- src/subbutton&notemod.js
+++ src/subbutton&notemod.js
@@ -601,7 +601,7 @@ function checkReimb(year, month, nonReimbCell, checkOrRes, typeSheet, specSheet,
 
   //adds into array where only non-reimbed items exist w/ their respective costs
   while (monthRowInd <= monthEndRow) {
-    if (specSheet.getRange(monthRowInd, reimbMarkColSpec).getValue() == false && !isBlank(specSheet.getRange(monthRowInd, totCostColSpec))) {
+    if (!specSheet.getRange(monthRowInd, reimbMarkColSpec).getValue() && !specSheet.getRange(monthRowInd, totCostColSpec).isBlank()) {
       Logger.log("month " + monthRowInd + " " + specSheet.getRange(monthRowInd, reimbMarkColSpec).getValue());
       nonReimbArray.push(specSheet.getRange(monthRowInd, totCostColSpec).getValue() + ": " + specSheet.getRange(monthRowInd, expTypeColSpec).getValue());
     }
