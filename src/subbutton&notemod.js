a84433098353cb26ade9900f7357dfe33067e6e7
unknown <eky5074@psu.edu>
2025-01-10 02:33:18 -0500

    catch error changes

--- src/subbutton&notemod.js
+++ src/subbutton&notemod.js
@@ -110,7 +110,7 @@ function onEdit(e) {
   }
   catch (error) {
     //Log the error to all error msg cells
-    SpreadsheetApp.getUi().alert(error);
+    SpreadsheetApp.getUi().alert("Error: " + error.message + "\n\n" + error.stack);
 
     //Reset all buttons
     redOutButton.setValue(false);
